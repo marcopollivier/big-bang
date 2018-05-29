9094e00b31623d7ad1b75fdd65d64178d261ac3f
Marco Paulo Ollivier <marcopollivier@gmail.com>
2018-05-29 00:27:13 -0300

    Altera script install

--- install.sh
+++ install.sh
@@ -4,36 +4,41 @@ echo "##################################"
 echo "###     Instalando Ansible     ###"
 echo "##################################"
 
-sudo apt update
-sudo apt upgrade
-sudo apt install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt install -y ansible
+#sudo apt update
+#sudo apt upgrade
+#sudo apt install -y software-properties-common
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt install -y ansible
 
 echo "###################################"
 echo "###  Aplicando scripts Ansible  ###"
 echo "###################################"
 
-ansible-playbook -k -b --ask-become-pass ansible/basic-env-install.yml
-ansible-playbook ansible/dev-env-install.yml
-ansible-playbook ansible/tool-env-install.yml
-ansible-playbook ansible/util-env-install.yml
-ansible-playbook ansible/study-env-install.yml
+exec-ansible-playbook() {
+    echo "Executando o playbook $1"
+    ansible-playbook -k -b --ask-become-pass ansible/$1
+}
+
+exec-ansible-playbook basic-env-install.yml
+exec-ansible-playbook dev-env-install.yml
+exec-ansible-playbook tool-env-install.yml
+exec-ansible-playbook util-env-install.yml
+exec-ansible-playbook study-env-install.yml
 
 #Docker Install
-ansible-playbook ansible/docker-install.yml
+exec-ansible-playbook docker-install.yml
 
 #Ubuntu
 if [ $(lsb_release -is) = "Ubuntu" ]
 then
     echo "Dependencia especifica Ubuntu"
-    ansible-playbook ansible/ubuntu-env-install.yml
+    exec-ansible-playbook ubuntu-env-install.yml
 fi
 
 #Ubuntu GNOME
 if [ $XDG_CURRENT_DESKTOP = "ubuntu:GNOME" ]
 then
     echo "Dependencia especifica Gnome"
-    ansible-playbook ansible/gnome-env-install.yml
+    exec-ansible-playbook gnome-env-install.yml
 fi
 
