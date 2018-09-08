90ec443bd07f63e20488e72601c398387c7d5f5f
Marco Ollivier <marcopollivier@gmail.com>
2018-09-08 01:31:38 -0300

    Reorganiza estrutura de execucao

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+name=$(uname -n)
 release=$(lsb_release -is)
 xdg=$XDG_CURRENT_DESKTOP
 
-# echo "##################################"
-# echo "###     Instalando Ansible     ###"
-# echo "##################################"
+# echo "##########################################################"
+# echo "###     Configurando ambiente de dev no host $name     ###"
+# echo "##########################################################"
 
 #sudo apt update
 #sudo apt upgrade
@@ -13,36 +14,21 @@ xdg=$XDG_CURRENT_DESKTOP
 #sudo apt-add-repository ppa:ansible/ansible
 #sudo apt install -y ansible
 
-# echo "###################################"
-# echo "###  Aplicando scripts Ansible  ###"
-# echo "###################################"
-
-# exec-ansible-playbook() {
-#     echo "Executando o playbook $1"
-#     ansible-playbook -k -b --ask-become-pass ansible/$1
-# }
-
-# exec-ansible-playbook basic-env-install.yml
-# exec-ansible-playbook dev-env-install.yml
-# exec-ansible-playbook tool-env-install.yml
-# exec-ansible-playbook util-env-install.yml
-# exec-ansible-playbook study-env-install.yml
-
-#Docker Install
-# exec-ansible-playbook docker-install.yml
-
 #Ubuntu
-if [[ $release = "Ubuntu" ]]; then
-    echo "Instalando dependências especifica UBUNTU"
-    # exec-ansible-playbook ubuntu-env-install.yml
-elif [[ $release = "ManjaroLinux" ]]; then
-  echo "Instalando dependências especifica MANJARO"
-  #statements
-fi
+# if [[ $release = "Ubuntu" ]]; then
+#     echo "Instalando dependências especifica $release"
+# elif [[ $release = "ManjaroLinux" ]]; then
+#   echo "Instalando dependências especifica $release"
+# fi
 
 #GNOME
-if [[ $xdg = *"GNOME"* ]];
-then
-    echo "Instalando dependências especifica GNOME"
-    # exec-ansible-playbook gnome-env-install.yml
-fi
+# if [[ $xdg = *"GNOME"* ]];
+# then
+#     echo "Instalando dependências especifica GNOME"
+#     # exec-ansible-playbook gnome-env-install.yml
+# fi
+
+ansible-playbook \
+-e var_deploy_release=$release \
+-k -b --ask-become-pass \
+ansible/install.yml
