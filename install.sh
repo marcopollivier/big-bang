0e235b8f91e89e3a71d11aa2155222b7818a40b6
Marco Paulo Ollivier <marcopollivier@gmail.com>
2018-09-09 23:48:16 -0300

    Organiza script, corrige pontos com falhas e configura parametrizacao das informacoes de config do git

--- install.sh
+++ install.sh
@@ -1,38 +1,64 @@
 #!/bin/bash
 
-name=$(uname -n)
-user=$(id -u -n)
-release=$(lsb_release -is)
-xdg=$XDG_CURRENT_DESKTOP
+function pre_config() {
+    if [[ $release = "Ubuntu" ]]; then
 
-echo "##########################################################"
-echo "###     Configurando ambiente de dev no host $name     ###"
-echo "##########################################################"
+        echo "not implemented"
+        #sudo apt list --installed | grep -i apache
 
-#sudo apt update
-#sudo apt upgrade
-#sudo apt install -y software-properties-common
-#sudo apt-add-repository ppa:ansible/ansible
-#sudo apt install -y ansible
+        #sudo apt update
+        #sudo apt upgrade
+        #sudo apt install -y software-properties-common
+        #sudo apt-add-repository ppa:ansible/ansible
+        #sudo apt install -y ansible
 
-#Ubuntu
-# if [[ $release = "Ubuntu" ]]; then
-#     echo "Instalando dependências especifica $release"
-# elif [[ $release = "ManjaroLinux" ]]; then
-#   echo "Instalando dependências especifica $release"
-# fi
+    elif [[ $release = "ManjaroLinux" ]]; then
 
-#GNOME
-# if [[ $xdg = *"GNOME"* ]];
-# then
-#     echo "Instalando dependências especifica GNOME"
-#     # exec-ansible-playbook gnome-env-install.yml
-# fi
+        if pacman -Qs ansible > /dev/null ; then
+          sudo pacman -Syu
+          sudo pacman -S ansible --noconfirm
+        fi
 
-ansible-playbook \
--e var_deploy_release=$release \
--e var_deploy_user=$user \
--e var_deploy_git_name='Marco Ollivier' \
--e var_deploy_git_email='marcopollivier@gmail.com' \
--k -b --ask-become-pass \
-ansible/install.yml
+    fi
+}
+
+if [ -z "$1" -o  $# -lt 2 ]; then
+    echo "Nenhum argumento informado e/ou argumento(s) vazios ou nulos"
+    echo "      ./$(basename $0) <nome do usuario git> <email usuario git>"
+    echo ""
+    echo "      ./$(basename $0) 'Fulano da Silva' 'fulano@gmail.com'"
+else
+    echo "#############################################################"
+    echo "           Configurando ambiente de dev no host $name        "
+    echo "#############################################################"
+    echo ""
+
+    pre_config
+
+    HOSTNAME=$(uname -n)            # nome do host da máquina
+    USERNAME=$(id -u -n)            # nome do usuario autenticado
+    DISTRONAME=$(lsb_release -is)   # nome da distro Ex.: Ubuntu, ManjaroLinux
+    XDGNAME=$XDG_CURRENT_DESKTOP    # nome da interface grafica usada Ex.: GNOME
+
+    GITNAME=$1
+    GITEMAIL=$2
+
+    echo "# DISTRONAME: $DISTRONAME"
+    echo "# USERNAME: $USERNAME"
+    echo "# XDGNAME: $XDGNAME"
+    echo "# GITNAME: $GITNAME"
+    echo "# GITEMAIL: $GITEMAIL"
+
+    echo -n "Are you sure? [ENTER]"
+    read SURE
+
+    ansible-playbook \
+    -e var_deploy_release="$DISTRONAME" \
+    -e var_deploy_user="$USERNAME" \
+    -e var_deploy_xdg="$XDGNAME" \
+    -e var_deploy_git_name="$GITNAME" \
+    -e var_deploy_git_email="$GITEMAIL" \
+    -k -b --ask-become-pass \
+    ansible/install.yml
+
+fi
